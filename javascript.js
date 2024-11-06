4b957f08b15d4a7997172d5f57551d764f474bab
ind3ci5ive <39619831+ind3ci5ive@users.noreply.github.com>
2024-11-05 18:25:53 -0600

    Add functions to get computer and player choices

--- javascript.js
+++ javascript.js
@@ -1,5 +1,25 @@
+
+// const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+// let humanScore = 0;
+// let computerScore = 0;
+
+
+
+
+//#region debug
 console.log("Hello");
 console.log(getComputerChoice());
+console.log(getHumanChoice());
+//#endregion
+
+
+
+
+
+
+
+
 
 
 
@@ -8,6 +28,7 @@ console.log(getComputerChoice());
 // Check which answer the user picks and which the computer picks (Rock > Scissors, Paper > Rock, Scissors > Paper)
 // Print whether the player won or lost
 
+//#region getComputerChoice
 function getComputerChoice() {
 
     let computerChoice = Math.floor(Math.random() * 3);
@@ -27,3 +48,91 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+//#endregion
+
+//#region getHumanChoice
+
+function getHumanChoice() {
+
+
+    do {
+
+
+        let userInput = prompt("Please choose Rock, Paper, or Scissors.");
+
+        if (userInput.toLowerCase() == "rock") {
+
+            return "Rock"; 
+        }
+
+        else if (userInput.toLowerCase() == "paper") {
+
+            return "Paper";
+        }
+
+        else if (userInput.toLowerCase() == "scissors") {
+
+            return "Scissors";
+        }
+
+        else {
+
+            console.log("Please provide proper input: Rock, Paper, or Scissors");
+        }
+
+    }
+    while(true);
+
+}
+//#endregion
+
+//#region playRound
+function playRound(computerSelection, humanSelection) {
+
+    if(computerSelection == "Rock" && humanSelection == "Rock"){
+
+        console.log("It's a tie! No points awarded!");
+    }
+
+    else if(computerSelection == "Rock" && humanSelection == "Paper"){
+
+        console.log("You win! Paper beats Rock.");
+    }
+
+    else if(computerSelection == "Rock" && humanSelection == "Scissors"){
+
+        console.log("You lose! Rock beats Scissors.");
+    }
+
+    else if(computerSelection == "Paper" && humanSelection == "Rock"){
+
+        console.log("You lose! Paper beats Rock.");
+    }
+
+    else if(computerSelection == "Paper" && humanSelection == "Paper"){
+
+        console.log("It's a tie! No points awarded!");
+    }
+
+    else if(computerSelection == "Paper" && humanSelection == "Scissors"){
+
+        console.log("You Win! Scissors beats Paper.");
+    }
+
+    else if(computerSelection == "Scissors" && humanSelection == "Rock"){
+
+        console.log("You win! Rock beats Scissors.");
+    }
+
+    else if(computerSelection == "Scissors" && humanSelection == "Paper"){
+
+        console.log("You lose! Scissors beats Paper");
+    }
+
+    else if(computerSelection == "Scissors" && humanSelection == "Scissors"){
+
+        console.log("It's a tie! No points awarded!");
+    }
+
+}
+//#endregion
